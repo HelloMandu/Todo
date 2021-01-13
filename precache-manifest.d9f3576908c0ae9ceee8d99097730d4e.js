1169efecd2a558dc3a099c0e05bba490e3bbde26
HelloMandu <tjdals6695@gmail.com>
2021-01-13 18:41:56 +0900

    Updates

---
+++ precache-manifest.d9f3576908c0ae9ceee8d99097730d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94344d3247bab264e20c3615d0d01c6f",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "4a352f6425f44986bf0d",
+    "url": "/Todo/static/css/main.6b1cdec6.chunk.css"
+  },
+  {
+    "revision": "5e69a822547a83124d80",
+    "url": "/Todo/static/js/2.1c745d0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Todo/static/js/2.1c745d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a352f6425f44986bf0d",
+    "url": "/Todo/static/js/main.37407d5d.chunk.js"
+  },
+  {
+    "revision": "40b31628a98602283aff",
+    "url": "/Todo/static/js/runtime-main.4c66f946.js"
+  }
+]);
